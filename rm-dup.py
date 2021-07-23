bbced5da3c44e52c2f1d23c0b93e24f1da436ca6
Kellie Grieneisen <kellie_grieneisen@aol.com>
2021-07-23 17:36:50 +0000

    part one of removing duplicates finished, refining and working on part 2.

---
+++ rm-dup.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        # this code allocates extra space for a second array
+        # check_nums=[]
+        # for num in nums:
+        #     if num not in check_nums:
+        #         check_nums.append(num)
+        #     else:
+        #         num=None
+                
+        # nums = check_nums
+        # return f'{len(nums)}, {sorted(nums)}'
+
+        # solve without allocating extra space to run in O(1)
+        
