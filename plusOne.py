61a8a0bed97dcfc8aa6c99db3dc2bb793c198bd2
Kellie Grieneisen <kellie_grieneisen@aol.com>
2021-07-26 15:44:58 +0000

    plus one challenge completed and accepted by leetcode

--- plusOne.py
+++ plusOne.py
@@ -4,13 +4,25 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        digits[-1]=digits[-1]+1
-        if digits[-1] > 9:
-            digits[-1]=str(digits[-1])
-            pt1 = digits[-1][0].split()
-            pt2= digits[-1][1].split()
-            print(pt1,pt2)
-            digits[-1]=int(pt1[0])
-            digits.append(int(pt2[0]))
+        # digits[-1]=digits[-1]+1
+        # if digits[-1] > 9:
+        #     digits[-1]=str(digits[-1])
+        #     pt1 = digits[-1][0].split()
+        #     pt2= digits[-1][1].split()
+        #     print(pt1,pt2)
+        #     digits[-1]=int(pt1[0])
+        #     digits.append(int(pt2[0]))
+        number=''
+        for digit in digits:
+            number = number + str(digit)
+        number= int(number) + 1
+        digits=[]
+        for i in str(number):
+            digits.append(int(i))
 
+        
         return digits
+
+    # accepted by leetcode !! 
+    #  24 ms, faster than 41.64% of Python online submissions for Plus One.
+    # 13.4 MB, less than 71.00% of Python online submissions for Plus One.
