4086941c4f263376cbfddb8913959b094617e250
Kellie Grieneisen <kellie_grieneisen@aol.com>
2021-07-27 19:57:03 +0000

    first commit

---
+++ same_tree.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+       
+        # Tree nodes dont support indexing so techinically, this doesnt work
+
+        # if p[1] == q[1] and p[2] == q[2]:
+        #     print("same tree")
+        #     return True
+        # return False
+
+        
