d365a555957e4cf34b590ec53bd89bf27946b702
Kellie Grieneisen <kellie_grieneisen@aol.com>
2021-08-18 03:57:44 +0000

    new challenge added.

---
+++ countemployees.py
@@ -0,0 +1,65 @@
+"""Count employees in an org chart.
+
+Our organization has the following org chart::
+
+                    Jane
+          Jessica          Janet
+       Al  Bob  Jen     Nick  Nora
+                                Henri
+
+Let's make this chart::
+
+    >>> henri = Node("Henri")
+    >>> nora = Node("Nora", [henri])
+    >>> nick = Node("Nick")
+    >>> janet = Node("Janet", [nick, nora])
+    >>> al = Node("Al")
+    >>> bob = Node("Bob")
+    >>> jen = Node("Jen")
+    >>> jessica = Node("Jessica", [al, bob, jen])
+    >>> jane = Node("Jane", [jessica, janet])
+
+And test our counting function::
+
+    >>> henri.count_employees()
+    0
+
+    >>> nora.count_employees()
+    1
+
+    >>> jane.count_employees()
+    8
+
+"""
+# We provide a non-recursive version, let's make sure that gives the same
+# answer::
+
+    # >>> jane.count_employees_nonrecursive()
+    # 8
+
+
+class Node(object):
+    """Node in a tree."""
+
+    def __init__(self, name, children=None):
+        self.name = name
+        self.children = children or []
+
+    def count_employees(self):
+        """Return a count of how many employees this person manages.
+
+        Return a count of how many people that manager manages. This should
+        include *everyone* under them, not just people who directly report to
+        them.
+        """
+        count = 0
+        for self.children in self.name.children:
+            count =count+1
+        print(count)
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. YOU ARE A TREE GENIUS!\n")
