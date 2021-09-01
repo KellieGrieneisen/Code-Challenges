28e0618dd4d9ce770d6c242de78855302157997f
Kellie Grieneisen <kellie_grieneisen@aol.com>
2021-09-01 17:17:28 +0000

    new hackerank challenge completed

---
+++ count_valleys.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    # Write your code here
+    
+    valley=0 
+    sea_level=0
+    
+    for ch in path:
+        if ch == "U":
+            sea_level+=1
+            if sea_level==0:
+                valley+=1
+        else:
+            sea_level -= 1
+            
+
+    return valley
+            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
