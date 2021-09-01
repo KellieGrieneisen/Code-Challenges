451153b6bfc9cdac55ad5e41a909e17e8455fd70
Kellie Grieneisen <kellie_grieneisen@aol.com>
2021-09-01 19:41:23 +0000

    new challenge completed on hackerrank

---
+++ jumping_clouds.py
@@ -0,0 +1,43 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'jumpingOnClouds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY c as parameter.
+#
+
+def jumpingOnClouds(c):
+    # Write your code here
+
+    jumps=0
+    clouds=0
+    
+    while clouds < len(c)-1:
+        clouds+=2
+        if clouds >= len(c) or c[clouds]==1:
+            clouds-=1
+        jumps += 1
+    
+    return jumps
+               
+
+        
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
