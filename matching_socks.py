9e34ee9e98be9a752c84480d420e1dca266b4ca0
Kellie Grieneisen <kellie_grieneisen@aol.com>
2021-09-02 19:13:26 +0000

    updating matching_socks challenge. all tests passed!

--- matching_socks.py
+++ matching_socks.py
@@ -18,14 +18,18 @@ def sockMerchant(n, ar):
     pairs=[]
     seen=[]
     count=0
-    
-    for i,num in enumerate(ar):
-        count = ar.count(num)
-        if num not in seen:
-            pairs.append(math.floor(count/2))
-        seen.append(ar.pop(i))
-        
+    x=0   
+    while x in range(0,len(ar)):
+        for num in ar:
+            count = ar.count(num)
+      
+            if num not in seen:
+                pairs.append(math.floor(count/2))
+                seen.append(num)
+            x=x+1
             
+            if x==len(ar):
+                return sum(pairs)
         
     return sum(pairs)
                 
