9059123a2c7a8fe671aa6b549db261afa55b3ade
Kellie Grieneisen <kellie_grieneisen@aol.com>
2021-09-13 16:12:39 +0000

    swapping challenge, runs but needs optimization.

---
+++ min_swaps.py
@@ -0,0 +1,22 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumSwaps function below.
+def minimumSwaps(arr):
+
+    # this code works but times out for some test runs
+    # working on optimizing
+    swap=0
+    current=0
+    for i,num in enumerate(arr):
+        
+        if num != i+1:
+            current =num
+            i_swap=arr.index(i+1)
+            arr[i]=arr[i_swap]
+            arr[i_swap]=current
+            swap +=1
+    return swap
