c6652d6294bae58d51844f4b24a74041d9bdb536
Kellie Grieneisen <kellie_grieneisen@aol.com>
2021-10-04 20:49:17 +0000

    substring check challenge

---
+++ checkmagazines.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'checkMagazine' function below.
+#
+# The function accepts following parameters:
+#  1. STRING_ARRAY magazine
+#  2. STRING_ARRAY note
+#
+
+def checkMagazine(magazine, note):
+    # Write your code here
+    
+    sub = ''.join(word for word in note if word in set(magazine))
+    if sub == str(note):
+        print("Yes")
+    else:
+        print("No")
+        
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    m = int(first_multiple_input[0])
+
+    n = int(first_multiple_input[1])
+
+    magazine = input().rstrip().split()
+
+    note = input().rstrip().split()
+
+    checkMagazine(magazine, note)
